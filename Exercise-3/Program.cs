80596390ef7661f7aa40e3eaccf6d6caf2ba612b
ASUS ROG <ASUS ROG@LAPTOP-TF4J2JH4>
2022-11-21 15:49:34 +0700

    Add project files.

---
+++ Exercise-3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_3
+{
+    class Node
+    {
+        /*creates Nodes forthe circular nexted list*/
+        public int noMhs;
+        public string name;
+        // point to the succeeding node
+        public Node next;
+        // point to the preccedding node
+        public Node prev;
+
+    }
+    class 
+    {
+
+    }
+}
